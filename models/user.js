3a1aac0c77b2e0a56efeeca95a920448a2880ace
Quan Vo <votrungquan99@gmail.com>
2020-03-10 19:29:30 -0400

    add access token to user model

--- models/user.js
+++ models/user.js
@@ -1,22 +1,28 @@
-const joi = require("joi");
-const mongoose = require('mongoose')
+const joi = require('joi');
+const mongoose = require('mongoose');
 
 // joi schema
 const schema = joi.object().keys({
-    username: joi.string().required(),
-    password: joi.string().required()
+	username: joi.string().required(),
+	password: joi.string().required()
 });
 
 // mongoose schema
 var userSchema = new mongoose.Schema({
-    username: String,
-    password: String
-})
+	username: String,
+	password: String,
+	access_token: [
+		{
+			value: String,
+			expireDate: Date
+		}
+	]
+});
 
-let user = mongoose.model('user', userSchema)
+let user = mongoose.model('user', userSchema);
 
 // exports
 module.exports = {
-    JoiModel: schema,
-    MongoModel: user
-}
+	JoiModel: schema,
+	MongoModel: user
+};
