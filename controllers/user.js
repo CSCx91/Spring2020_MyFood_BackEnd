a80e275ba3cf211d45e7368801c71832858c9970
Derek Raushi <draushi99@gmail.com>
2020-03-12 12:15:32 -0400

    wrap create user into function

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
 var mongoUser = require('../models/user').MongoModel;
+var mongoUser = require('../models/user').MongoModel;
+var joiUser = require('../models/user').JoiModel;
+var sha256 = require('js-sha256').sha256;
 
 let getUserById = async id => {
 	try {
@@ -11,4 +14,52 @@ let getUserById = async id => {
 	}
 };
 
+let createUser = async (data) => {
+
+	try {		
+		let validationResult = joiUser.validate(data);
+		// check if there is an error
+		if (validationResult.error) {
+			return({ status: 'fail', message: 'valition error' });
+		}
+	} catch (error) {
+		console.log('---------------------error at validate create user function------------')
+		console.log(error);
+		return({ status: 'fail', message: 'validation error' });
+	}
+
+	
+
+	// checking if user is already in db
+	try {
+		let existUser = await mongoUser.findOne({ username: data.username });
+		if (existUser) {
+			return({ status: 'fail', message: 'user exists' });
+		}
+		
+	} catch (error) {
+		console.log('---------------------error at check database for user in create user function------------')
+		console.log(error);
+		return({ status: 'fail', message: 'Connection issue' });
+	}
+
+	let { password, ...userInfo } = data;
+	password = sha256(password);
+	let newUser = new mongoUser({ ...userInfo, password, access_token: [] });
+
+	// check if there is an error when adding to database
+	try {
+		
+		await newUser.save();
+	} catch (error) {
+		console.log(error);
+			console.log('---------------------error at new user save in create user function------------')
+			console.log(error);
+			return({ status: 'fail', message: "can't create user" });
+	}
+	return {status: 'successful'}
+
+}
+
 exports.getUserById = getUserById;
+exports.createUser = createUser;
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mongoUser = require('../models/user').MongoModel;
-var joiUser = require('../models/user').JoiModel;
-var sha256 = require('js-sha256').sha256;
 var userController = require('../controllers/user');
 
 var getUserById = userController.getUserById;
+var createUser = userController.createUser;
 
 /* GET users listing. */
 router.get('/:id', async (req, res, next) => {
@@ -29,44 +27,16 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async function(req, res) {
 	const body = req.body;
-	let validationResult = joiUser.validate(body);
-	// check if there is an error
-	if (validationResult.error) {
-		res.send({ status: 'fail', message: 'valition error' });
-		return;
+	// let {username, password} = req.body;
+	try {
+		let result = await createUser(body);
+		res.send(result);
+	} catch (error) {
+		console.log('---------------------error at create user route-----------------')
+		console.log(error)
+		res.send({status: 'fail', message: 'system error'});
 	}
 
-	let existUser;
-
-	// checking if user is already in db
-	await mongoUser.findOne({ username: body.username }, (err, user) => {
-		if (err) {
-			res.send({
-				status: 'fail',
-				message: 'can not check if user exist'
-			});
-			return;
-		}
-		existUser = user;
-	});
-	if (existUser) {
-		res.send({ status: 'fail', message: 'user exists' });
-		return;
-	}
-
-	let { password, ...userInfo } = body;
-	password = sha256(password);
-	let newUser = new mongoUser({ ...userInfo, password, access_token: [] });
-
-	// check if there is an error when adding to database
-	newUser.save(err => {
-		if (err) {
-			console.log(err);
-			res.send({ status: 'fail', message: "can't create user" });
-			return;
-		}
-		res.send({ status: 'success' });
-	});
 });
 
 module.exports = router;
