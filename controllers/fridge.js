89f60d7444ba7daa33587103d599c8f1c4da92ef
Derek Raushi <draushi99@gmail.com>
2020-03-31 12:19:48 -0400

    added fridge

--- controllers/fridge.js
+++ controllers/fridge.js
@@ -1,2 +1,46 @@
-var mongoUser = require('../models/fridge').MongoModel;
-var joiUser = require('../models/fridge').JoiModel;
+var mongoFridge = require('../models/fridge').MongoModel;
+var joiFridge = require('../models/fridge').JoiModel;
+
+let getFridgeById = async id => {
+	try {
+		let user = await mongoFridge.findOne({
+			_id: id
+		});
+		return fridge;
+	} catch (error) {
+		return null;
+	}
+};
+
+let createFridge = async (data) => {
+
+	try {		
+		let validationResult = joiFridge.validate(data);
+		// check if there is an error
+		if (validationResult.error) {
+			return({ status: 'fail', message: 'valition error' });
+		}
+	} catch (error) {
+		console.log('---------------------error at validate create user function------------')
+		console.log(error);
+		return({ status: 'fail', message: 'validation error' });
+    }
+
+    let newFridge = new mongoFridge({ id, freezer, cooler});
+
+	// check if there is an error when adding to database
+	try {
+		
+		await newFridge.save();
+	} catch (error) {
+		console.log(error);
+			console.log('---------------------error at new fridge save in create fridge function------------')
+			console.log(error);
+			return({ status: 'fail', message: "can't create fridge" });
+	}
+	return {status: 'successful'}
+
+}
+
+exports.getFridgeById = getFridgeById;
+exports.createFridge = createFridge;
--- routes/fridge.js
+++ routes/fridge.js
@@ -1,3 +1,41 @@
 var express = require('express');
 var router = express.Router();
-//var userController = require('../controllers/fridge');
+var userController = require('../controllers/fridge');
+
+var getFridgeById = fridgeController.getFridgeById;
+var createFridge = fridgeController.createFridge;
+
+router.get('/:id', async (req,res, next) => {
+    let id = req.params.id;
+    try {
+        let fridge = await getFridgeById(id)
+        if (fridge) {
+            let {id, freezer, cooler} = user._doc;
+            res.send(resFridge)
+        }
+            else {
+                res.send(null)
+            }
+        } catch (error) {
+            console.log(
+                '--------------------error at get fridge route---------------------'
+            );
+            console.log(error);
+            res.send(null);
+        }
+    });
+
+    router.post('/', async function(req, res) {
+       const body = req.body;
+       try {
+           let result = await createFridge(body);
+           res.send(result);
+       }catch (error) {
+		console.log('---------------------error at create fridge route-----------------')
+		console.log(error)
+		res.send({status: 'fail', message: 'system error'});
+	}
+
+});
+
+module.exports = router;
